d2a73823c3e439d3089e721e095cbe65336e7be1
anirudh302 <anirudhtupe3@gmail.com>
2023-08-14 16:45:39 +0530

    today

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Images from "./../assets/images/bg/dev-ft-bg.jpg"
+// import Images from "./../assets/images/bg/dev-ft-bg.jpg"
 
 const Footer = () => {
     return <>
         {/* <div className=""> */}
         <div className="mx-8 bg-gray-950 mt-20">
+            {/* <div className="border-2"></div> */}
             <div className="relative">
                 {/* <img src={Images} alt="" /> */}
                 <div className="">
@@ -22,6 +23,7 @@ const Footer = () => {
                                     <i class="bi bi-instagram border rounded-2xl px-2 hover:bg-yellow-400 hover:text-black py-1 text-center"></i>
                                     <i class="bi bi-youtube border rounded-2xl px-2 hover:bg-yellow-400 hover:text-black py-1 text-center"></i>
                                 </div>
+                                    {/* <div className="border mt-5"></div> */}
                             </div>
                         </div>
                         <div className="lg:col-span-3 col-span-12">
@@ -33,6 +35,7 @@ const Footer = () => {
                                     <h1 className=' lg:mt-2 mt-1 hover:text-yellow-400'>Blog</h1>
                                     <h1 className=' lg:mt-2 mt-1 hover:text-yellow-400'>Contact Us</h1>
                                 </div>
+                                {/* <div className="border mt-10"></div> */}
                             </div>
                         </div>
                         <div className="lg:col-span-3 col-span-12">
@@ -44,10 +47,11 @@ const Footer = () => {
                                     <h1 className='mt-2 hover:text-yellow-400'>Track Your Order</h1>
                                     <h1 className='mt-2 hover:text-yellow-400'>Trade Program</h1>
                                 </div>
+                                {/* <div className="border mt-10"></div> */}
                             </div>
                         </div>
                         <div className="lg:col-span-3 col-span-12">
-                            <div className="lg:py-20 px-10">
+                            <div className="lg:py-20 px-5">
                                 <h1 className='text-white text-3xl font-bold'>Official Info:</h1>
                                 <div className="mt-14 text-white font-bold flex gap-2">
                                     <i class="bi bi-geo-alt-fill text-yellow-400"></i>
@@ -61,11 +65,13 @@ const Footer = () => {
                                     <h1>Subscribe Newsletter</h1>
                                 </div>
                                 <div className="border-none">
-                                    <input type="text" className='px-1 py-3 rounded mt-3 text-center' placeholder='Emali Address' /> <button className='py-3 px-1 rounded bg-yellow-400 text-white'>Subscribe</button>
+                                    <input type="text" className='lg:px-1 lg:py-3 rounded mt-3 text-center' placeholder='Emali Address' /> <button className='lg:py-3 px-1 rounded bg-yellow-400 text-white'>Subscribe</button>
                                 </div>
+                                {/* <div className="border"></div> */}
                             </div>
                         </div>
                     </div>
+                    {/* <div className="border-2"></div> */}
                 </div>
             </div>
         </div>
