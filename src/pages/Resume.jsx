3802d9d3461fc1767c46f7d3701b9110a656b58e
anirudh302 <anirudhtupe3@gmail.com>
2023-08-16 16:42:34 +0530

    3454

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -78,7 +78,7 @@ const Resume = () => {
                         <h1 className='text-3xl font-bold text-gray-500'>Skills</h1>
                         <div className="border mt-2 "></div>
                         <div className="flex gap-2 mt-4 font-bold">
-                            <h1 className='border hover:bg-yellow-400 text-white hover:text-black transition duration-300  lg:py-3 lg:px-3 rounded-lg  p-2'>Html</h1>
+                            <h1 className='border hover:bg-yellow-400 text-white hover:text-black transition duration-300  lg:py-3 lg:px-3 rounded-lg'>Html</h1>
                             <h1 className='border hover:bg-yellow-400 text-white hover:text-black transition duration-300  lg:py-3 lg:px-3 rounded-lg p-2'>CSS</h1>
                             <h1 className='border hover:bg-yellow-400 text-white hover:text-black transition duration-300  lg:py-3 lg:px-3  rounded-lg p-2'>Front-end</h1>
                             <h1 className='border hover:bg-yellow-400 text-white hover:text-black transition duration-300  lg:py-3 lg:px-3 p-2 rounded-lg'>PHP</h1>
